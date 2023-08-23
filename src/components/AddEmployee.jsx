e3c888fa0496a348f4d236ad8e536a4982b8fca6
adarshgupta404 <gadarsh747@gmail.com>
2023-08-24 04:51:52 +0530

    commit

--- src/components/AddEmployee.jsx
+++ src/components/AddEmployee.jsx
@@ -33,7 +33,7 @@ const AddEmployee = () => {
     }
   return (
     <div className="p-10 pt-2 font-semibold flex justify-center w-100">
-      <form className="md:w-96" onSubmit={handleSubmit} method="post">
+      <form className="md:w-96" onSubmit={handleSubmit}>
         <h3 className="py-2">Add Employee</h3>
         <div className="mb-6">
           <label
@@ -132,7 +132,7 @@ const AddEmployee = () => {
         />
 
         <button
-          type="submit"
+          type="submit" onClick={console.log("clicked")}
           className="text-white mt-6 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         >
           Register new employee
