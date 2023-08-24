07dc87194b8b43c84bac1266fce2f7887e21ba86
Adarsh Gupta <112967201+adarshgupta404@users.noreply.github.com>
2023-08-24 23:33:19 +0530

    Update baseUrl.js

--- src/components/baseUrl.js
+++ src/components/baseUrl.js
@@ -1,3 +1,4 @@
 // apiConfig.js
-const baseUrl = "https://ems-backend-eeqw.onrender.com";
+// const baseUrl = "https://ems-backend-eeqw.onrender.com";
+const baseUrl = "https://ems-backend-backup-production.up.railway.app";
 export default baseUrl;
