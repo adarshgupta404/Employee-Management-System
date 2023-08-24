7e9ac4053d992d5a7487296514e56abda4d31d0f
adarshgupta404 <gadarsh747@gmail.com>
2023-08-24 06:10:08 +0530

    minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,35 @@
 import React, {lazy, Suspense} from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import LoginPage from "./components/loginPage";
-import SignupPage from "./components/signupPage";
+// import LoginPage from "./components/loginPage";
+// import SignupPage from "./components/signupPage";
 // import Dashboard from "./components/Dashboard";
 // import Employee from "./components/Employee";
-import Profile from "./components/Profile";
-import Home from "./components/Home";
+// import Profile from "./components/Profile";
+// import Home from "./components/Home";
+// import AddEmployee from "./components/AddEmployee";
+// import EditEmployee from "./components/EditEmployee";
 import "./index.css";
-import AddEmployee from "./components/AddEmployee";
-import EditEmployee from "./components/EditEmployee";
-const LazyDashboard = lazy(()=>import('./components/Dashboard'))
+const Dashboard = lazy(()=>import('./components/Dashboard'))
 const Employee = lazy(()=>import('./components/Employee'))
+const Home = lazy(()=>import('./components/Home'))
+const Profile = lazy(()=>import('./components/Profile'))
+const AddEmployee = lazy(()=>import('./components/AddEmployee'))
+const EditEmployee = lazy(()=>import('./components/EditEmployee'))
+const LoginPage = lazy(()=>import('./components/loginPage'))
+// const SignupPage = lazy(()=>import('./components/signupPage'))
+
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Suspense fallback={<div>Please Wait...</div>}><LazyDashboard /></Suspense>}>
+        <Route path="/" element={<Suspense fallback={<div className="w-screen h-screen flex justify-center items-center"><div>Please Wait...</div></div>}><Dashboard /></Suspense>}>
           <Route path="" element={<Home/>}></Route>
           <Route path="/employee" element={<Suspense fallback={<div>Please Wait...</div>}><Employee/></Suspense>}></Route>
-          <Route path="/profile" element={<Profile/>}></Route>
-          <Route path="/create" element={<AddEmployee/>}></Route>
-          <Route path="/editemployee/:id" element={<EditEmployee/>}></Route>
+          <Route path="/profile" element={<Suspense fallback={<div>Please Wait...</div>}><Profile/></Suspense>}></Route>
+          <Route path="/create" element={<Suspense fallback={<div>Please Wait...</div>}><AddEmployee/></Suspense>}></Route>
+          <Route path="/editemployee/:id" element={<Suspense fallback={<div>Please Wait...</div>}><EditEmployee/></Suspense>}></Route>
         </Route>
-        <Route path="/login" element={<LoginPage />}></Route>
+        <Route path="/login" element={<Suspense fallback={<div>Please Wait...</div>}><LoginPage/></Suspense>}></Route>
       </Routes>
     </BrowserRouter>
   );
