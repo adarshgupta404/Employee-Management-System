fa4962f46bb41a0e62ac5d11d39cca2ffe0f8c04
adarshgupta404 <gadarsh747@gmail.com>
2023-08-24 06:01:38 +0530

    minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,21 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginPage from "./components/loginPage";
 import SignupPage from "./components/signupPage";
 // import Dashboard from "./components/Dashboard";
-import Employee from "./components/Employee";
+// import Employee from "./components/Employee";
 import Profile from "./components/Profile";
 import Home from "./components/Home";
 import "./index.css";
 import AddEmployee from "./components/AddEmployee";
 import EditEmployee from "./components/EditEmployee";
 const LazyDashboard = lazy(()=>import('./components/Dashboard'))
+const Employee = lazy(()=>import('./components/Employee'))
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Suspense><LazyDashboard /></Suspense>}>
+        <Route path="/" element={<Suspense fallback={<div>Please Wait...</div>}><LazyDashboard /></Suspense>}>
           <Route path="" element={<Home/>}></Route>
-          <Route path="/employee" element={<Employee/>}></Route>
+          <Route path="/employee" element={<Suspense fallback={<div>Please Wait...</div>}><Employee/></Suspense>}></Route>
           <Route path="/profile" element={<Profile/>}></Route>
           <Route path="/create" element={<AddEmployee/>}></Route>
           <Route path="/editemployee/:id" element={<EditEmployee/>}></Route>
